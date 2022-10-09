741921394721c10ebfa8303a41d6d07c0ccc2680
Aleksander Sushchenko <039pro@gmail.com>
2022-10-09 18:33:36 +0200

    Great

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 receptu = []
-with open('recept.txt' ,'r', encoding='utf8') as f:
+with open('recept.txt', 'r', encoding='utf8') as f:
     for line in f:
         bludo = line.strip()
-        recept = {'naz_bludo': bludo, 'kol':[]}
+        recept = {'naz_bludo': bludo, 'kol': []}
         kol_ingr = f.readline()
         for i in range(int(kol_ingr)):
-            s=f.readline()
+            s = f.readline()
             ingr, kol, ed_izm = s.split(' | ')
             sostav = {'ingr': ingr, 'kol': kol, 'ed_izm': ed_izm}
             recept['kol'].append(kol)
         f.readline()
         receptu.append(recept)
-
-
-
-
 
 print(recept)
