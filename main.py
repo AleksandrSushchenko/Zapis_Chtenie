0f6c9b786cd7114816bfd70246d7b0126b2873a1
Aleksander Sushchenko <039pro@gmail.com>
2022-10-11 23:28:56 +0200

    Разобратся с циклом, почему задвоение?

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-receptu = []
+print('________________Задача 1_____________________')
+cook_book = {}
 with open('recept.txt', 'r', encoding='utf8') as f:
     for line in f:
-        bludo = line.strip()
-        recept = {'naz_bludo': bludo, 'kol': []}
-        kol_ingr = f.readline()
-        for i in range(int(kol_ingr)):
-            s = f.readline()
-            ingr, kol, ed_izm = s.split(' | ')
-            sostav = {'ingr': ingr, 'kol': kol, 'ed_izm': ed_izm}
-            recept['kol'].append(kol)
+        naz_bludo = line
+        bludo = []
+        kol_indig = f.readline()
+        for i in range(int(kol_indig)):
+            stroka = f.readline()
+            produkt, kol, ed_izm = stroka.split(' | ')
+            sostav_produkta = {'indigrient_name': produkt, 'quantity': kol, 'measure': ed_izm.strip()}
+            bludo.append(sostav_produkta)
+        recept = {line.strip(): bludo}
         f.readline()
-        receptu.append(recept)
+        cook_book.update(recept)
 
-print(recept)
+print(cook_book)
+print('________________Задача 2_____________________')
+
+e = {}
+dishes = []
+def get_shop_list_by_dishes(dishes, person_count):
+    for j in range(len(dishes)):
+        if dishes[j] in cook_book.keys():
+            for i in cook_book[dishes[j]]:
+                p = {'quantity': int(i.get('quantity')) * int(person_count), 'measure': i.get('measure')}
+                f = {i.get('indigrient_name'): p}
+                e.update(f)
+            print(e)
+        else:
+            print('no')
+
+get_shop_list_by_dishes(['Омлет','Фахитос','Утка по-пекински'], 10)
