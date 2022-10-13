e6fd701e3bf6224771b0592a88484a0fa3a2d9d6
Aleksander Sushchenko <039pro@gmail.com>
2022-10-13 22:05:25 +0200

    Остановился на записи

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 print('________________Задача 1_____________________')
 cook_book = {}
 with open('recept.txt', 'r', encoding='utf8') as f:
@@ -36,4 +38,35 @@ def get_shop_list_by_dishes(dishes, person_count):
 get_shop_list_by_dishes(['Омлет', 'Фахитос'], 3)
 
 print('________________Задача 3_____________________')
+
+s={}
+with open('1.txt', 'r', encoding='utf8') as f1:
+    s1 = {os.path.basename('1.txt') : len(f1.readlines())}
+    s.update(s1)
+
+with open('2.txt', 'r', encoding='utf8') as f2:
+    s2 = {os.path.basename('2.txt') : len(f2.readlines())}
+    s.update(s2)
+
+with open('3.txt', 'r', encoding='utf8') as f3:
+    s3 = {os.path.basename('3.txt') : len(f3.readlines())}
+    s.update(s3)
+
+sorted_tuples = sorted(s.items(), key=lambda item: item[1])
+#sorted_dict = {k: v for k, v in sorted_tuples}
+print(sorted_tuples)
+with open('4.txt', 'w') as p:
+    a = sorted_tuples[0][0]
+    b = sorted_tuples[0][1]
+    print(a)
+    p.write(a)
+
+
+
+
+
+
+
+
+
 
